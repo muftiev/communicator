d7ee3bf315a0c2900971b1c34295de6766f8b287
Sergey Tkachenko <re4thzdsq@gmail.com>
2014-03-05 18:15:27 +0200

    change base

--- chat/app.js
+++ chat/app.js
@@ -1,71 +1,14 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var config = require('config');
-var log = require('lib/log')(module);
-var mongoose = require('lib/mongoose');
-var HttpError = require('error').HttpError;
-
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.engine('ejs', require('ejs-locals'));
-app.set('views', __dirname + '/template');
-app.set('view engine', 'ejs');
+app.use(logfmt.requestLogger());
 
-app.use(express.favicon());
-
-if (app.get('env') == 'development') {
-  app.use(express.logger('dev'));
-} else {
-  app.use(express.logger('default'));
-}
-
-app.use(express.bodyParser());
-
-app.use(express.cookieParser());
-
-var sessionStore = require('lib/sessionStore');
-
-app.use(express.session({
-  secret: config.get('session:secret'),
-  key: config.get('session:key'),
-  cookie: config.get('session:cookie'),
-  store: sessionStore
-}));
-
-app.use(require('middleware/sendHttpError'));
-app.use(require('middleware/loadUser'));
-
-app.use(app.router);
-
-require('routes')(app);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(function(err, req, res, next) {
-  if (typeof err == 'number') {
-    err = new HttpError(err);
-  }
-
-  if (err instanceof HttpError) {
-    res.sendHttpError(err);
-  } else {
-    if (app.get('env') == 'development') {
-      express.errorHandler()(err, req, res, next);
-    } else {
-      log.error(err);
-      err = new HttpError(500);
-      res.sendHttpError(err);
-    }
-  }
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-
-var server = http.createServer(app);
-server.listen(config.get('port'), function(){
-  log.info('Express server listening on port ' + config.get('port'));
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
-var io = require('./socket')(server);
-app.set('io', io);
